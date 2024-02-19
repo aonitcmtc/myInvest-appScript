66ed5892bfe4753f5e6aef3054ccd1326c659b61
aonitcmtc <patchara_aon_1@hotmail.com>
2024-02-20 02:13:21 +0700

    update script decode

--- html/js/script/my_load_header.js
+++ html/js/script/my_load_header.js
@@ -2,14 +2,14 @@
 // jquery_dataTables_min_js.src = '../../myInvest-appScript/assets/node_modules/datatables.net/js/jquery.dataTables.min.js';
 // document.head.appendChild(jquery_dataTables_min_js);
 
-// <!-- CRYPTO-JS  -->
-var crypto_js = document.createElement('script');
-crypto_js.src = '../assets/plugins/encryp/crypto-js.min.js';
-document.head.appendChild(crypto_js);
-// <!-- JWT-TOKEN  -->
-var jwt_token = document.createElement('script');
-jwt_token.src = '../assets/plugins/encryp/jwt_token.js';
-document.head.appendChild(jwt_token);
+// // <!-- CRYPTO-JS  -->
+// var crypto_js = document.createElement('script');
+// crypto_js.src = '../assets/plugins/encryp/crypto-js.min.js';
+// document.head.appendChild(crypto_js);
+// // <!-- JWT-TOKEN  -->
+// var jwt_token = document.createElement('script');
+// jwt_token.src = '../assets/plugins/encryp/jwt_token.js';
+// document.head.appendChild(jwt_token);
 // <!-- DATE-PICKER  -->
 var bootstrap_datepicker = document.createElement('link');
 bootstrap_datepicker.rel = 'stylesheet';
